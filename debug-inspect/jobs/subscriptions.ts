43128961cdabb7797fbb15c1615f6902263a8503
Zorux <developers@zorux.dev>
2026-05-20 13:46:21 -0300

    chore: remove debug artifacts

--- debug-inspect/jobs/subscriptions.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  name: "check-expired-subscriptions",
-  async perform(_args: any) {
-    console.log("Checking for expired subscriptions...")
-  },
-}
--- test-e2e/seed.ts
+++
@@ -1,5 +0,0 @@
-import { F } from "zorux"
-
-export default async function seed() {
-  console.log("  Seed data created!")
-}
